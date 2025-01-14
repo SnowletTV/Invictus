a451b06a737abc3a4e4747ee80ee4b7eae50b3c6
Anbeeld <anbeeld@gmail.com>
2025-01-14 21:02:36 +0100

    Hardcoded the values and added comments

---
+++ tools/inventions/invention_relative_chance.js
@@ -0,0 +1,60 @@
+// Steps to generate script values for
+let percents = [
+  5,
+  10,
+  15,
+  20,
+  25,
+  30,
+  35,
+  40,
+  45,
+  50,
+  55,
+  60,
+  65,
+  70,
+  75,
+  80,
+  85,
+  90,
+  95,
+  99,
+  100
+];
+
+function roundFloat(value, precision) {
+  return parseFloat(value.toFixed(precision));
+}
+
+// Number of inventions that the country can choose out from at the moment
+// Hardcoded because tracking it for every country just for a bit more precision is too much
+// See more in /common/inventions/invention_relative_chance.info
+const num_inventions_to_choose_from = 20;
+
+function calculateValue(percent) {
+  // For 100% just get some really big number to soft-guarantee the innovation getting chosen
+  if (percent === 100) {
+    return 99999999;
+  }
+  // Otherwise calculate the chance. It's a simple calculation where we have x elements of 1 weight each, and then we
+  // need to make one of the elements of a certain weight that would become x percent of the new combined weight.
+  return (num_inventions_to_choose_from - 1) / (1 - percent / 100) - (num_inventions_to_choose_from - 1);
+}
+
+let script_values = ``;
+
+for (let percent of percents) {
+  // Add the script value itself
+  script_values += `irc_${percent} = ${roundFloat(calculateValue(percent), 4)}\n`;
+
+  // Add the multipliers so we could upgrade this percent to any of the higher percents
+  for (let target of percents) {
+    if (percent >= target) {
+      continue;
+    }
+    script_values += `irc_from_${percent}_to_${target} = ${roundFloat(calculateValue(target) / calculateValue(percent), 4)}\n`;
+  }
+}
+
+console.log(script_values.trim());
